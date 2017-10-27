a1bd14417814266f7fa6d1a455a7e1d9819e3d70
Neil Padgen <neil.padgen@autodata-group.com>
2017-10-27 17:05:34 +0100

    Accessing NSSpeechSynthesizer framework.  This might give more reliable and more interesting behaviour than command line 'say'.

---
+++ nsspeechsynthesizer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# https://stackoverflow.com/questions/12758591/python-text-to-speech-in-macintosh#12761406
+
+from  AppKit import NSSpeechSynthesizer
+import time
+import sys
+
+
+def print_and_say(text):
+    words = text.split(' ')
+    ve.startSpeakingString_(text)
+    while not ve.isSpeaking():
+        time.sleep(0.05)
+    while len(words) > 0:
+        sys.stdout.write(words.pop(0) + ' ')
+        sys.stdout.flush()
+        ve.pauseSpeakingAtBoundary_(1)
+        while ve.isSpeaking():
+            time.sleep(0.05)
+        ve.continueSpeaking()
+        time.sleep(0.05)
+
+
+if len(sys.argv) < 2:
+   text = input('type text to speak> ')
+else:
+   text = ' '.join(sys.argv[1:])
+
+nssp = NSSpeechSynthesizer
+
+ve = nssp.alloc().init()
+
+for voice in nssp.availableVoices():
+   ve.setVoice_(voice)
+   print(voice)
+   ve.startSpeakingString_(text)
+
+   while not ve.isSpeaking():
+      time.sleep(0.1)
+
+   while ve.isSpeaking():
+      time.sleep(0.1)
