b6af6bb784caccddb97deebd7ca21bf4d1d91c08
npadgen <neil.padgen@gmail.com>
2023-10-12 17:20:55 +0100

    Migrated to macos_speech from pyttx3 (#29)

--- read_a_script/script_learner.py
+++ read_a_script/script_learner.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+#
+# pylint: disable=line-too-long
 
 """Usage:
   script_learner.py [-h] [-c CONFIG_FILE] [-dqLV] -r ROLE [-r ROLE]... [-s SCENES] SCRIPT_FILE
@@ -42,29 +44,31 @@ options:
   learning-method: PAUSE_AND_DISPLAY
 """
 
-from .utils import ElementType, mixrange
-from jouvence.parser import JouvenceParser
 import enum
 import os
 import re
-import readchar
-import logging
 import subprocess
-import docopt
 import sys
+
+import docopt
+import readchar
+from jouvence.parser import JouvenceParser
+from loguru import logger
+from macos_speech import Synthesizer, Voice
 from ruamel.yaml import YAML
-import pyttsx3
+
+from read_a_script.utils import ElementType, mixrange
 
 ACTION_CHARACTER = "_ACTION"
 DEFAULT_CHARACTER = "_DEFAULT"
 DEFAULT_CONFIG = __doc__.split("---\n")[1]
-LOGGER = logging.getLogger(sys.argv[0])
-LOGGER.addHandler(logging.StreamHandler())
 
 DEFAULT_VOICE = "Daniel"
 
 
 class LearningMethod(enum.Enum):
+    """enum"""
+
     PAUSE_AND_DISPLAY = enum.auto()
     DISPLAY_AND_PAUSE = enum.auto()
     WAIT_FOR_INPUT = enum.auto()
@@ -77,26 +81,30 @@ class Actor:
     An Actor displays lines that it is given, while reading them out in its selected voice.
     """
 
-    def __init__(self, config, role, voice, engine: pyttsx3.Engine):
+    def __init__(self, config, role, voice: Voice):
         self.role = role
         self.voice = voice
-        self.engine = engine
+        self.synth = Synthesizer(voice=voice.name)
         self.config = config
+        if "rate" in self.config["options"]:
+            self.synth.rate = int(self.config["options"]["rate"])
 
     def read_line(self, line):
+        "Display a line and speak it aloud."
         self.display_line(line)
         self.speak_line(line)
 
     def speak_line(self, line):
+        "Speak a line of action aloud."
         if self.role == ACTION_CHARACTER and not self.config["options"].get(
             "speak-action", True
         ):
             return
-        self.engine.setProperty("voice", self.voice.id)
-        self.engine.say(line)
-        self.engine.runAndWait()
+        if line:
+            self.synth.say(line)
 
     def display_line(self, line, include_character=True):
+        "Display a line of action without speaking it."
         if (
             self.role is not None
             and self.role != ACTION_CHARACTER
@@ -106,6 +114,7 @@ class Actor:
         print(line.strip() + "\n")
 
     def display_character(self):
+        "Display the character's name."
         print(f"{self.role.upper()}: ", end="", flush=True)
 
 
@@ -127,9 +136,14 @@ class LearningActor(Actor):
         try:
             self.learning_method = LearningMethod[lm]
         except KeyError:
-            self.learning_method = LearningMethod(int[lm])
+            self.learning_method = LearningMethod(int(lm))
         if self.learning_method == LearningMethod.WAIT_FOR_INPUT:
             self.print_help_interactive()
+
+    def _mute_unmute_output(self, muted: bool):
+        subprocess.run(
+            ["osascript", "-e", f"set volume output muted {muted}"], check=False
+        )
 
     def silent_speak_line(self, line):
         """
@@ -137,12 +151,11 @@ class LearningActor(Actor):
         :param line:
         :return:
         """
-        self.engine.setProperty("voice", self.voice.id)
-        volume = self.engine.getProperty("volume")
-        self.engine.setProperty("volume", 0)
-        self.engine.say(line)
-        self.engine.runAndWait()
-        self.engine.setProperty("volume", volume)
+        if not line:
+            return
+        self._mute_unmute_output(True)
+        self.synth.say(line)
+        self._mute_unmute_output(False)
 
     def speak_line(self, line):
         if self.learning_method == LearningMethod.SPEAK_AND_DISPLAY:
@@ -151,19 +164,22 @@ class LearningActor(Actor):
             return self.silent_speak_line(line)
 
     def read_line_pause_then_display(self, line):
+        """Silently read the line, then display it"""
         self.display_character()
         self.silent_speak_line(line)
         self.display_line(line, include_character=False)
 
     def read_line_display_then_pause(self, line):
+        """Display a line, then silently read it"""
         self.display_line(line)
         self.silent_speak_line(line)
 
     def read_line_as_actor(self, line):
-        super(LearningActor, self).read_line(line)
+        """Read the line in the way the superclass would read it"""
+        super().read_line(line)
 
     def read_line_interactive(self, line):
-        self.engine.setProperty("voice", self.voice.id)
+        """Read the line one word at a time"""
         self.display_character()
         while True:
             sys.stdout.flush()
@@ -177,25 +193,27 @@ class LearningActor(Actor):
                     hint, line = re.split(r"\s+", line, 1)
                 else:
                     hint, line = line, None
-                self.engine.say(hint)
-                self.engine.runAndWait()
+                self.synth.say(hint)
                 sys.stdout.write(hint + " ")
                 sys.stdout.flush()
                 if line is None:
                     sys.stdout.write("\n")
                     return
-            elif say_it == " " or say_it == "n":
+            elif say_it in (" ", "n"):
                 print(line)
                 return
-            elif say_it == "\x013" or say_it == "y":
+            elif say_it in ("\x013", "y"):
                 print(line)
-                self.engine.say(line)
-                self.engine.runAndWait()
+                self.synth.say(line)
                 return
             else:
                 self.print_help_interactive()
 
     def print_help_interactive(self):
+        """
+        Give some help to the poor frustrated actor
+        who can't remember how to use interactive mode
+        """
         print()
         print("  Hit H for a hint, enter or Y to read the whole line,")
         print("  space or N to skip to the next line without reading,")
@@ -211,10 +229,15 @@ class LearningActor(Actor):
         elif self.learning_method == LearningMethod.SPEAK_AND_DISPLAY:
             self.read_line_as_actor(line)
         else:
-            LOGGER.warning(f"Unknown learning method {self.learning_method}!")
+            logger.warning(f"Unknown learning method {self.learning_method}!")
 
 
+# pylint: disable=too-many-instance-attributes
 class ScriptReciter:
+    """
+    Read out the script
+    """
+
     def __init__(self, script_file, roles, config):
         self.parser = JouvenceParser()
         self.d = self.parser.parse(script_file)
@@ -224,15 +247,13 @@ class ScriptReciter:
         self.current_role = None
         self.current_actor = None
 
-        self.engine: pyttsx3.Engine = pyttsx3.init()
-        if "rate" in self.config["options"]:
-            self.engine.setProperty("rate", int(self.config["options"]["rate"]))
-        self.voices = dict(
-            (v.name.capitalize(), v) for v in self.engine.getProperty("voices")
-        )
+        self.voices = dict((v.name.capitalize(), v) for v in Synthesizer().voices)
         self.actors = {}
 
     def learn(self, scenes=None):
+        """
+        Learn the selected scenes
+        """
         print("You are learning: " + ", ".join(self.roles))
         if scenes is None:
             scenes = self.d.scenes
@@ -242,17 +263,26 @@ class ScriptReciter:
             self.learn_scene(scene)
 
     def list_scenes(self):
+        """
+        List all the scenes in the play
+        """
         for i, scene in enumerate(self.d.scenes, 1):
-            print("{:-8d}: {}".format(i, scene.header))
+            print(f"{i:-8d}: {scene.header}")
 
     def list_voices(self):
+        """
+        List all the voices installed on this machine
+        """
         print("Known voices: ")
         for voice in self.voices.values():
-            print(f"\t{voice.name} ({', '.join(voice.languages)})")
+            print(f"\t{voice.name} (f{voice.lang})")
 
     def learn_scene(self, scene):
+        """
+        Learn an individual scene
+        """
         self.current_actor = self.get_actor(ACTION_CHARACTER)
-        self.current_actor.read_line("Scene: " + scene.header)
+        self.current_actor.read_line("Scene: " + (scene.header or ""))
         for p in scene.paragraphs:
             p_type = ElementType(p.type)
             if p_type in (
@@ -272,7 +302,10 @@ class ScriptReciter:
             else:
                 self.get_actor(DEFAULT_CHARACTER).read_line(p.text)
 
-    def get_actor(self, character_name):
+    def get_actor(self, character_name) -> Actor:
+        """
+        Get the Actor object for the given character
+        """
         if character_name is not None:
             character_name = character_name.strip()
         if character_name in self.actors:
@@ -282,35 +315,41 @@ class ScriptReciter:
             if voice_name in self.voices:
                 voice = self.voices[voice_name]
             else:
-                LOGGER.warning(
+                logger.warning(
                     f"{voice_name} is not in the list of known voices - using {DEFAULT_VOICE}"
                 )
                 voice = self.voices[DEFAULT_VOICE]
         else:
             if character_name != ACTION_CHARACTER:
-                LOGGER.warning(
+                logger.warning(
                     f"Could not find {character_name} in configuration.voices - using {DEFAULT_VOICE}"
                 )
             voice = self.voices[DEFAULT_VOICE]
         if character_name is None:
-            actor = Actor(self.config, None, voice, self.engine)
+            actor = Actor(self.config, None, voice)
         elif character_name in self.roles:
-            actor = LearningActor(self.config, character_name, voice, self.engine)
+            actor = LearningActor(self.config, character_name, voice)
         else:
-            actor = Actor(self.config, character_name, voice, self.engine)
+            actor = Actor(self.config, character_name, voice)
         self.actors[character_name] = actor
         return actor
 
 
+@logger.catch
 def main():
+    """
+    The show must go on
+    """
     opts = docopt.docopt(__doc__, sys.argv[1:])
 
     if "--config" in opts and os.path.exists(opts["--config"]):
+        # pylint: disable=W1514
         config = YAML().load(open(opts["--config"]).read())
     else:
         config = YAML().load(DEFAULT_CONFIG)
 
     if DEFAULT_CHARACTER in config["voices"]:
+        # pylint: disable=W0603
         global DEFAULT_VOICE
         DEFAULT_VOICE = config["voices"][DEFAULT_CHARACTER]
     learner = ScriptReciter(opts["SCRIPT_FILE"], opts["--role"], config)
